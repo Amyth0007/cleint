f56eb78b84938617a6dd0fb7c817753e74e131b0
Amit manore <96157492+Amyth0007@users.noreply.github.com>
2025-08-12 17:53:58 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   googleMapsApiKey: 'AIzaSyA1TTUD5luKbZoBrDRdEOI-qEeF1ZL5XzI', // Replace with your actual API key
-  apiUrl: 'https://backend-postgress.onrender.com/api'
+  apiUrl1: 'https://backend-postgress.onrender.com/api',
+  apiUrl: 'http://16.171.6.207/api'
   // apiUrl: 'http://localhost:3003/api'
 }; 
